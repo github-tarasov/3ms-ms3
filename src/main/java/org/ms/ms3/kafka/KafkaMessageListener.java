d915b90a2ecafb1aeea46b67fa275c97d857ed3b
Anton Tarasov <ATarasov@bi-telco.com>
2025-01-25 21:23:50 +0300

    Fix Jaeger config

--- src/main/java/org/ms/ms3/kafka/KafkaMessageListener.java
+++ src/main/java/org/ms/ms3/kafka/KafkaMessageListener.java
@@ -4,7 +4,6 @@ import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.instrumentation.annotations.SpanAttribute;
-import io.opentelemetry.instrumentation.annotations.WithSpan;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ms.dto.Message;
--- src/main/java/org/ms/ms3/kafka/OpenTelemetryConfiguration.java
+++ src/main/java/org/ms/ms3/kafka/OpenTelemetryConfiguration.java
@@ -1,25 +1,33 @@
-package org.ms.ms3.kafka;
+package org.ms.ms3.websocket;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.exporter.otlp.http.logs.OtlpHttpLogRecordExporter;
+import io.opentelemetry.exporter.otlp.http.metrics.OtlpHttpMetricExporter;
 import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
+
 import static io.opentelemetry.semconv.ServiceAttributes.SERVICE_NAME;
+
 
 @Configuration
 public class OpenTelemetryConfiguration {
 
-    @Value("${jaeger.server}")
-    private String jaegerServer;
+    @Value("${jaeger.host}")
+    private String jaegerHost;
 
     @Value("${spring.application.name}")
     private String springApplicationName;
@@ -27,18 +35,41 @@ public class OpenTelemetryConfiguration {
     @Bean
     public Tracer tracer() {
         SpanExporter spanExporter = OtlpHttpSpanExporter.builder()
-                .setEndpoint(jaegerServer)
+                .setEndpoint("http://" + jaegerHost + ":4318/v1/traces")
+                .setTimeout(Duration.ofSeconds(10))
                 .build();
         Resource resource = Resource.getDefault()
                 .merge(Resource.create(Attributes.of(SERVICE_NAME, springApplicationName)));
         SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-                .addSpanProcessor(BatchSpanProcessor.builder(spanExporter).build())
+                .addSpanProcessor(SimpleSpanProcessor.builder(spanExporter).build())
                 .setResource(resource)
                 .build();
-        OpenTelemetrySdk openTelemetry = OpenTelemetrySdk.builder()
-                .setTracerProvider(sdkTracerProvider)
-                .build();
-        GlobalOpenTelemetry.set(openTelemetry);
-        return openTelemetry.getTracer("scopeName");
+        OpenTelemetrySdk openTelemetrySdk =
+                OpenTelemetrySdk.builder()
+                        .setTracerProvider(sdkTracerProvider)
+                        .setMeterProvider(
+                                SdkMeterProvider.builder()
+                                        .registerMetricReader(
+                                                PeriodicMetricReader.builder(
+                                                                OtlpHttpMetricExporter.builder()
+                                                                        .setEndpoint("http://" + jaegerHost + ":4318/v1/metrics")
+                                                                        .build()
+                                                        )
+                                                        .setInterval(Duration.ofMillis(1000))
+                                                        .build()
+                                        ).build()
+                        )
+                        .setLoggerProvider(
+                                SdkLoggerProvider.builder()
+                                        .addLogRecordProcessor(
+                                                BatchLogRecordProcessor.builder(OtlpHttpLogRecordExporter.builder()
+                                                        .setEndpoint("http://" + jaegerHost + ":4318/v1/logs")
+                                                        .setTimeout(Duration.ofSeconds(10))
+                                                        .build()
+                                                ).build()
+                                        ).build()
+                        )
+                        .buildAndRegisterGlobal();
+        return openTelemetrySdk.getTracer("scopeName");
     }
 }
